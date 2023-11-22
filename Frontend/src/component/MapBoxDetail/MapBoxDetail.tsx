0f77a77920989701c908dd1844655577d6e5ea6a
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-22 13:28:26 +0700

    demo map detail

--- Frontend/src/component/MapBoxDetail/MapBoxDetail.tsx
+++ Frontend/src/component/MapBoxDetail/MapBoxDetail.tsx
@@ -6,7 +6,15 @@ function MapBoxDetail({ selected }: any) {
     return (
         <div className='map_box_container'>
             {/* {selected ? (selected.m_id) : (null)} */}
-            <div style={{ backgroundColor: "black", margin: "1rem" }}></div>
+            <div style={{ margin: "1rem", display: "flex", flexDirection: "column", gap: "1rem" }}>
+                <div>ฟุตบาตไม่เสมอ</div>
+                <div>[tag1,tag2]</div>
+                <div>wordwordwordwordwordwordwordwordwo
+                    rdwordwordwordwordwordwordwordwordw
+                </div>
+                <div>location</div>
+
+            </div>
             <div style={{ backgroundColor: "red", margin: "1rem" }}></div>
 
         </div>
