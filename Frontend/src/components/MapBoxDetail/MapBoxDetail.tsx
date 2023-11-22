f38ba3cf45915bd255837f884ae29ad281985d81
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-22 20:22:50 +0700

    improve map box detail follow design

--- Frontend/src/components/MapBoxDetail/MapBoxDetail.tsx
+++ Frontend/src/components/MapBoxDetail/MapBoxDetail.tsx
@@ -10,7 +10,7 @@ function TagBox({ text }: { text: string }) {
         background: "linear-gradient(0deg, #FDFDDB, #FDFDDB),linear-gradient(0deg, #FCFBB1, #FCFBB1)",
         border: "1px solid rgba(252, 251, 177, 1)",
         borderRadius: "2px",
-        frontSize: "12px",
+        fontSize: "12px",
 
 
         // Add any additional styling like font size, color, etc.
@@ -31,9 +31,9 @@ function MapBoxDetail({ selected }: { selected: MapDataLocationProps | null }) {
     return (
         <div className='map_box_container'>
             {/* {selected ? (selected.m_id) : (null)} */}
-            <div style={{ margin: "1rem", display: "flex", flexDirection: "column", gap: "8px" }}>
-                <div style={{ fontWeight: "bold" }}>ฟุตบาตไม่สม่ำเสมอ</div>
-                <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
+            <div style={{ margin: "1rem", display: "flex", flexDirection: "column", gap: "1rem" }}>
+                <div style={{ fontWeight: "bold" ,fontSize:"1rem" }}>ฟุตบาตไม่สม่ำเสมอ</div>
+                <div style={{ display: "flex",  flexWrap: "wrap" }}>
                     {lst_tag.map((item) => {
                         return (
                             <TagBox text={item} />
@@ -43,8 +43,9 @@ function MapBoxDetail({ selected }: { selected: MapDataLocationProps | null }) {
                 </div>
 
                 <div style={{
-                    display: "flex", flexWrap: "wrap", wordWrap: "break-word", wordBreak: "break-all",
-                }}>ฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลย</div>
+                    flexWrap: "wrap", wordWrap: "break-word", wordBreak: "break-all", textOverflow: " ellipsis", display: "-webkit-box", WebkitBoxOrient: "vertical", WebkitLineClamp: 3, overflow: "hidden", fontSize: "14px"
+
+                }}>ฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุdwkdowjodjwodjwodjowjdowjdowjdojwodjwodjwตบาทไม่สม่ำเสมอเลยฟตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลย</div>
                 <div style={{ color: "#00000073" }}>ถนนงามวงศ์วาน</div>
             </div>
             <VoteBox points={points} />
