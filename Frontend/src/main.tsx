6d36a46d7c04f6b5fef7a1e7a21cfc73e36cf03e
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-22 11:47:29 +0700

    init map

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,11 +4,18 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import Alert_map from './page/Alert_map';
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/alert-map',
+    element: <Alert_map />,
+  },
+  
   
 ]);
 
