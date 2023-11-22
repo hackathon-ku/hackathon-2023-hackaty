238f0dcf3f73a32e25dd2ac272a1058da2579510
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-22 19:20:01 +0700

    add vote report feature

--- Frontend/src/component/MapBoxDetail/MapBoxDetail.tsx
+++ Frontend/src/component/MapBoxDetail/MapBoxDetail.tsx
@@ -5,13 +5,13 @@ import VoteBox from '../VoteBox';
 
 function TagBox({ text }: { text: string }) {
     const style = {
-        width: '46px',
-        height: '22px',
         padding: '1px 8px',
-        borderRadius: '2px',
         gap: '3px',
         background: "linear-gradient(0deg, #FDFDDB, #FDFDDB),linear-gradient(0deg, #FCFBB1, #FCFBB1)",
-        border: "1px solid rgba(252, 251, 177, 1)"
+        border: "1px solid rgba(252, 251, 177, 1)",
+        borderRadius: "2px",
+        frontSize: "12px",
+
 
         // Add any additional styling like font size, color, etc.
     };
@@ -27,11 +27,12 @@ function TagBox({ text }: { text: string }) {
 
 function MapBoxDetail({ selected }: { selected: MapDataLocationProps | null }) {
     const lst_tag = ["tag1", "tag2", "tag3"]
+    const points = selected?.vote_score || 0;
     return (
         <div className='map_box_container'>
             {/* {selected ? (selected.m_id) : (null)} */}
-            <div style={{ margin: "1rem", display: "flex", flexDirection: "column", gap: "1rem" }}>
-                <div style={{ fontWeight: "bold" }}>ฟุตบาตไม่เสมอ</div>
+            <div style={{ margin: "1rem", display: "flex", flexDirection: "column", gap: "8px" }}>
+                <div style={{ fontWeight: "bold" }}>ฟุตบาตไม่สม่ำเสมอ</div>
                 <div style={{ display: "flex", gap: "8px", flexWrap: "wrap" }}>
                     {lst_tag.map((item) => {
                         return (
@@ -42,11 +43,11 @@ function MapBoxDetail({ selected }: { selected: MapDataLocationProps | null }) {
                 </div>
 
                 <div style={{
-                    display: "flex", flexWrap: "wrap", wordWrap: "break-word"
-                }}>kfpefepkfwkfowkfowkfowkokokfpekf</div>
-
+                    display: "flex", flexWrap: "wrap", wordWrap: "break-word", wordBreak: "break-all",
+                }}>ฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลยฟุตบาทไม่สม่ำเสมอเลย</div>
+                <div style={{ color: "#00000073" }}>ถนนงามวงศ์วาน</div>
             </div>
-            <VoteBox />
+            <VoteBox points={points} />
 
 
         </div>
--- Frontend/src/component/VoteBox.tsx
+++ Frontend/src/component/VoteBox.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { UpOutlined, DownOutlined } from '@ant-design/icons';
 import { Button, Divider, Flex, Radio } from 'antd';
-function VoteBox() {
+function VoteBox({ points }: { points: number }) {
     return (
-    <div>
-        <Button type="primary" shape="circle" icon={<UpOutlined />} />
-        <div>12</div>
-        <div>คำร้อง</div>
-    </div>
+        <div style={{ gap: "8px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <Button  shape="circle" style={{ margin: "8px" }} icon={<UpOutlined />} size='large' />
+            <div style={{ gap: "8px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <div style={{ fontSize: "14px", color: "var(--primary-206, #030B0B)" }}>{points}</div>
+                <div style={{ fontSize: "12px", color: "#BFBFBF" }}>คำร้อง</div>
+            </div>
+            <Button  shape="circle" style={{ margin: "8px" }} icon={<DownOutlined />} size='large' />
+
+        </div>
     )
 }
-
 export default VoteBox
