09d048450935d2cb4706a9739b2e275a2d23cbd7
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-23 23:26:02 +0700

    remove unused thing

--- Backend/utils.py
+++ Backend/utils.py
@@ -4,13 +4,8 @@ from datetime import datetime
 
 # gmaps = googlemaps.Client(config("GOOGLE_MAP_API_KEY", cast=str))
 def calculate_distance_linear(lat1, lon1, lat2, lon2):
+    print(lat1, lon1)
     return distance((lat1, lon1), (lat2, lon2)).km
-
-
-def calculate_distance_road(lat1, lon1, lat2, lon2):
-    distance = gmaps.distance_matrix([str(lat1) + " " + str(lon1)], [str(lat2) + " " + str(lon2)], mode='walking')['rows'][0]['elements'][0]['distance']['text']
-    dis, unit = distance.split()
-    return round(float(dis), 2), unit
 
 
 def is_later_than(dt_str1, dt_str2):
