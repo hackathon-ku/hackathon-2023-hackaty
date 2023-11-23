f4db53409331d37630b6dff05e8ac62c94882077
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-23 21:56:39 +0700

    fix alert2

--- Backend/routers/ReportRouter.py
+++ Backend/routers/ReportRouter.py
@@ -4,6 +4,7 @@ from db import Report, Tag
 from datetime import datetime
 from utils import calculate_distance_linear, is_later_than
 import pytz
+from typing import Optional
 
 router = APIRouter(
     prefix="/api/report",
@@ -62,7 +63,7 @@ async def update_report(report_body: UpdateReportBody):
     body = report_body.model_dump()
     old_report = await Report.get(body['report_id'])
     if not old_report:
-        return HTTPException(f"Report id: {body['report_id']}  not found")
+        raise HTTPException(f"Report id: {body['report_id']}  not found")
     old_report.priority = body['priority']
     old_report.report_status = body['report_status']
     old_report.last_report_time = datetime.now()
@@ -85,29 +86,39 @@ async def update_vote_score(report_body: UpdateReportVoteBody):
         "message": f"report {body['report_id']} save successfully"
     }
 
+async def find_report(lat, lon):
+    lst = []
+    for report in await Report.find(Report.report_status=="Approved").to_list():
+        distance = calculate_distance_linear(lat, lon, report.lat, report.lon)
+        lst.append({**report.model_dump(), "distance": distance})
+
+    return lst
+
 
 @router.get('/get_alert/{last_report_timestamp}/{lat}/{lon}')
-async def get_alert(last_report_timestamp, lat, lon):
-    lst = []
-    if isinstance(last_report_timestamp, str):
-        last_report_timestamp = datetime.fromisoformat(last_report_timestamp[:-6])
-    last_reported = last_report_timestamp
-    all_report = await Report.find().to_list()
+async def get_alert(last_report_timestamp: Optional[str], lat, lon):
+    if len(last_report_timestamp) <= 2:
+        lst = await find_report(lat, lon)
+    else:
+        lst = []
+        if isinstance(last_report_timestamp, str) and last_report_timestamp != '':
+            last_report_timestamp = datetime.fromisoformat(last_report_timestamp[:-6])
+        last_reported = last_report_timestamp
+        all_report = await Report.find(Report.report_status=="Approved").to_list()
 
-    for report in all_report:
-        if report.last_report_time is None:
-            continue
-        if isinstance(last_reported, str) and last_reported != '':
-            last_reported = datetime.fromisoformat(last_reported[:-6])
-        if last_report_timestamp is not None:
+        for report in all_report:
+            if report.last_report_time is None:
+                continue
+            if isinstance(last_reported, str) and last_reported != '':
+                last_reported = datetime.fromisoformat(last_reported[:-6])
             distance = calculate_distance_linear(lat, lon, report.lat, report.lon)
-        if report.last_report_time > last_report_timestamp:
-            lst.append({**report.model_dump(), "distance": distance})
-            if report.last_report_time > last_reported:
-                last_reported = report.last_report_time
+            if report.last_report_time > last_report_timestamp:
+                lst.append({**report.model_dump(), "distance": distance})
+                if report.last_report_time > last_reported:
+                    last_reported = report.last_report_time
     return {
         "message": "success",
         "report": lst,
-        "last_report_timestamp": last_reported
+        "last_report_timestamp": max(report['last_report_time'] or datetime(0, 0, 0, 0, 0, 0, 0) for report in lst)
     }
 
