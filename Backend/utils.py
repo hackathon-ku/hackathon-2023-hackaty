84c08e03a67b301f632e5aca343e605eae873f31
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-23 17:29:23 +0700

    add endpoint

--- Backend/utils.py
+++ Backend/utils.py
@@ -2,6 +2,7 @@ from geopy.distance import distance
 import googlemaps
 from decouple import config
 from datetime import datetime
+import pytz
 
 gmaps = googlemaps.Client(config("GOOGLE_MAP_API_KEY", cast=str))
 def calculate_distance_linear(lat1, lon1, lat2, lon2):
@@ -16,8 +17,11 @@ def calculate_distance_road(lat1, lon1, lat2, lon2):
 
 def is_later_than(dt_str1, dt_str2):
     # Parse the strings into datetime objects
-    dt1 = datetime.fromisoformat(str(dt_str1))
-    dt2 = datetime.fromisoformat(str(dt_str2))
+
+    dt1 = datetime.fromisoformat(str(dt_str1)).astimezone(pytz.UTC)
+    dt2 = datetime.fromisoformat(str(dt_str2)).astimezone(pytz.UTC)
+    # dt1 = datetime.fromisoformat(str(dt_str1))
+    # dt2 = datetime.fromisoformat(str(dt_str2))
 
     return dt1 > dt2
 
