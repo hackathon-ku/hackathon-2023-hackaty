7b0b798e0651c8ad8a9423203a71f598a993b14a
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-23 23:01:18 +0700

    a

--- Backend/routers/ReportRouter.py
+++ Backend/routers/ReportRouter.py
@@ -101,7 +101,7 @@ async def get_alert(last_report_timestamp: Optional[str], lat, lon):
     else:
         lst = []
         if isinstance(last_report_timestamp, str) and last_report_timestamp != '':
-            last_report_timestamp = datetime.fromisoformat(last_report_timestamp[:-6])
+            last_report_timestamp = datetime.fromisoformat(last_report_timestamp[:-7])
         last_reported = last_report_timestamp
         all_report = await Report.find(Report.report_status=="Approved").to_list()
  
@@ -109,7 +109,7 @@ async def get_alert(last_report_timestamp: Optional[str], lat, lon):
             if report.last_report_time is None:
                 continue
             if isinstance(last_reported, str) and last_reported != '':
-                last_reported = datetime.fromisoformat(last_reported[:-6])
+                last_reported = datetime.fromisoformat(last_reported[:-7])
             distance = calculate_distance_linear(lat, lon, report.lat, report.lon)
             if report.last_report_time > last_report_timestamp:
                 lst.append({**report.model_dump(), "distance": distance})
