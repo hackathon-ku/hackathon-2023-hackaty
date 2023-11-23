7557764190ec6579a4b1c623717b94e8475d8769
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2023-11-23 22:58:13 +0700

    add

--- Backend/utils.py
+++ Backend/utils.py
@@ -1,11 +1,10 @@
 from geopy.distance import distance
-from decouple import config
 from datetime import datetime
 
 
 # gmaps = googlemaps.Client(config("GOOGLE_MAP_API_KEY", cast=str))
-# def calculate_distance_linear(lat1, lon1, lat2, lon2):
-#     return distance((lat1, lon1), (lat2, lon2)).km
+def calculate_distance_linear(lat1, lon1, lat2, lon2):
+    return distance((lat1, lon1), (lat2, lon2)).km
 
 
 def calculate_distance_road(lat1, lon1, lat2, lon2):
@@ -18,7 +17,5 @@ def is_later_than(dt_str1, dt_str2):
     # Parse the strings into datetime objects
 
     dt2 = datetime.fromisoformat(dt_str2)
-
-
     return dt_str1 > dt2
 
