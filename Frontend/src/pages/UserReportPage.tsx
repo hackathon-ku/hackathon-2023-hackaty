f1c6bf660b7b72a3d98c0761fe39d96acd055a60
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-11-23 10:19:49 +0700

    init UserReportForm

---
+++ Frontend/src/pages/UserReportPage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import StudentLayout from '../modules/common/StudentLayout'
+
+function UserReportPage() {
+  return (
+    <StudentLayout>
+      <div>UserReportPage</div>
+    </StudentLayout>
+  )
+}
+
+export default UserReportPage
